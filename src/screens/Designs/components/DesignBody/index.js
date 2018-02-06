3482d516777a2cbe1dd94edfd2dd96ff1c0b5aac
Pratama <pratamasetya99@gmail.com>
2018-02-06 21:35:55 +0700

    not yet done

--- src/screens/Designs/components/DesignBody/index.js
+++ src/screens/Designs/components/DesignBody/index.js
@@ -5,10 +5,18 @@ import {
   View,
   Text,
   StyleSheet,
+  TouchableOpacity
 } from 'react-native';
+import { Button } from 'native-base'
 import { connect } from 'react-redux'
+import Gestures from 'react-native-easy-gestures';
 
 class DesignBody extends Component {
+
+  state = {
+    styles : ''
+  }
+
   render() {
     console.log(this.props.textMenu);
     const { textMenu } = this.props
@@ -19,11 +27,23 @@ class DesignBody extends Component {
         <Text style={{padding:5}}>{textMenu.fontColor}</Text>
         <Text style={{padding:5}}>{textMenu.fontSize}</Text>
         <Text style={{padding:5}}>{textMenu.fontLength}</Text>
-        <View style={{width: fontLength}}>
+        <Text style={{borderWidth:1}}>asdasd</Text>
+        <Text style={{padding:5}}>{JSON.stringify(this.state.styles)}</Text>
+        <Gestures
+          styles={{width:fontLength}}
+          scalable={false}
+          onChange={(event, styles) => {
+            console.log(styles);
+            this.setState({
+              styles
+            })
+        }}>
+        <View style={{width:fontLength, borderWidth:2, borderColor:'red'}}>
           <Text style={{padding:5, fontFamily, fontSize, color:fontColor}}>
             Fifilio
           </Text>
         </View>
+        </Gestures>
       </View>
     );
   }
